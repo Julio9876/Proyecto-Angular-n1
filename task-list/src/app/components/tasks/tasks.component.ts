4226665296b824e9c879461d5144eef82c8e8c39
Julio9876 <juliogimenez.edc@gmail.com>
2023-01-16 03:05:39 -0300

    finalizado

--- task-list/src/app/components/tasks/tasks.component.ts
+++ task-list/src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { TaskService } from 'src/app/service/task.service';
 import { Task } from 'src/app/Task';
 
@@ -15,5 +16,23 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     //Like Promise
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task)
+      .subscribe(()=>(
+      this.tasks = this.tasks.filter( (t) => t.id !== task.id )
+    ))
+  }
+
+  toggleReminder(task:Task){
+  task.reminder = !task.reminder
+  this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task){
+    this.taskService.addTask(task).subscribe((task)=>(
+      this.tasks.push(task)
+    ))
   }
 }
